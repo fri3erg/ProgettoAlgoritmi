f70afe6bb8d9f939aac44e84784d32d650d70c35
fri3erg <elia.fri3erg@gmail.com>
2023-03-29 11:54:58 +0200

    matteo cazzi

--- Algoritmi2023/src/connectx/L0/L0.java
+++ Algoritmi2023/src/connectx/L0/L0.java
@@ -33,16 +33,149 @@ public class L0 implements CXPlayer {
 	}
 
 	public void initPlayer(int M, int N, int K,  boolean first, int timeout_in_secs) {
+		int max=0;
+		int[] eval= new int[M];
+		for(int i=0;i<M;i++) {
+			eval[i]=0;
+		}
 		// New random seed for each game
 		rand = new Random(System.currentTimeMillis());
 	}
 
 	/* Selects a random column */
 	public int selectColumn(CXBoard B) {
+		
 		//Integer[] L = B.getAvailableColumns();
 		//return L[rand.nextInt(L.length)];
+		if(numOfFreeCells()!=0) {					//if not full
+			if(currentPlayer()==0) { 				//if my turn
+				int [] column=getAvailableColumns();
+		for(int i=0;i<column.length;i++) {			//for each column
+			if(markColumn(column[i])==WIN1) {		//if i win						//not sure
+				return column[i];
+		}		
+		}
+		for(int repeat=0;repeat==0;repeat++) {		//if valid
+		repeat=0;
+		max=max(eval);
+		markColumn(max);							//mark the best
+		int [] column=getAvailableColumns();
+		for(int i=0;i<column.length;i++) {			//for each column
+			if(markColumn(column[i])==WIN2) {		//if he wins										//not sure
+				if(column[i]==max) {				//wins and same column
+					eval[column[i]]=-1;				//dont put that column
+					repeat--;						//try another column
+				}
+				else {
+				return column[i];					//if i have to block
+				}
+				}
+			
+				
+		}
+		}
+
+		int [] column=getAvailableColumns();
+		for(int i=0;i<column.length;i++) {			//for each column
+		if(checkForced(column[i], 0)==102) {		//check my forced
+			return column[i];
+		}
+		}
+		for(int repeat=0;repeat==0;repeat++) {		//if valid
+			max=max(eval);
+			repeat=0;
+			markColumn(max);						//mark the best
+		for(int i=0;i<column.length;i++) {			//for each column
+			if(checkForced(column[i], 1)==102) {	//if they can force 
+			eval[max]=-1;	
+			repeat--;								//try another column
+		}
+		}
+		}
+		if(max<=100) {
+			deepAnalysis();
+		}
+		}
+		}
+	}
+	
+	void deepAnalysis() {
+		
+	}
+	
+	int max(int[] eval) {
+		int maxfound=Math.max(eval);
+		if(maxfound<0) {
+			//porcodio
+		}
+		
+	}
+	
+	void generalCheck() {
+		int [] column=getAvailableColumns();
+		for(int i=0;i<column.length;i++) {
+		eval[column[i]]= rand.nextInt(100);
+		}
+		
+	}
+	
+	int checkForced(int column, int player) {
+			markColumn(column);
+			int forced =forcedColumn();
+			if(forced=-2) {
+				return 102;
+			}
+			if(!bool(forced+1)) {
+				checkForced(forced,int(!bool(player)));
+			}
+			else {
+				return 0;
+			}
+		}
+		
+		//for each
+		//mark me
+		//check if they need to put it there
+		//mark them
+		//repeat
+		//if 2 way/i win return 
+		//if gets to nothing sad return
 		
 		
+		
+		
+		
+		
+		
+		
+		return 0;
+		}
+		for(int i=0;i<column.length||i<2;i++) {
+		markColumn(i);
+		for(int j=0;j<getAvailableColumns().length;j++) {
+		if(markColumn(j)==WINmy) {}
+		return j;
+		}
+		unmarkColumn();
+		if(i==1) {
+			markColumn(0);
+			if(markColumn(1)==WINmy) {}
+			return 1;
+			}
+		return -1;
+		}
+		}
+	}
+		
+		int forcedColumn(){
+		for(int i=0;i<getAvailableColumns().length;i++) {
+			if (markColumn(i)==WIN?opp) {
+				return i;
+			}
+		}
+		return -1;
+		}
+		}
 		//mark each us
 		//check win if win(""104"" and break)
 		//check forced (forced tree if win 102(check accidental opponent win))
@@ -63,7 +196,7 @@ public class L0 implements CXPlayer {
 		//max every time(time safety) or at the end (time efficient)
 		//general cares of?? closeness??center??
 		//hard coded for beginning???
-	}
+	
 
 	public String playerName() {
 		return "enzo";
